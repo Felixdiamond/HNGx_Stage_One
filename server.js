a4b23bbc648bfeb60c8c0bb4394bde729fdf584a
Felixdiamond <diamondfelix006@gmail.com>
2023-09-08 02:32:57 +0100

    improve code

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const infoRoute = require('./routes/info');
+const path = require('path');
 
 app.use('/info', infoRoute);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
