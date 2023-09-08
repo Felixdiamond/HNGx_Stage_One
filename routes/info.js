1ba668e53ab9fc661ac88001478f78019319a5bd
Felixdiamond <diamondfelix006@gmail.com>
2023-09-08 15:57:33 +0100

    update code

--- routes/info.js
+++ routes/info.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
   const slack_name = req.query.slack_name;
   const track = req.query.track;
   const current_day = new Date().toLocaleString("en-US", { weekday: "long" });
-  const utc_time = new Date().toISOString();
+  const utc_time = new Date().toISOString().slice(0, -5) + 'Z';
   const github_file_url =
     "https://github.com/Felixdiamond/HNGx_Stage_One/blob/main/server.js";
   const github_repo_url = "https://github.com/Felixdiamond/HNGx_Stage_One";
