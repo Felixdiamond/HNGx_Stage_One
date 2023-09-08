40dd4a67502e7e78daf14d64c7a7e4396ee02a90
Felixdiamond <diamondfelix006@gmail.com>
2023-09-08 01:29:35 +0100

    update code

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const infoRoute = require('./routes/info');
 
 app.use('/info', infoRoute);
 
-app.listen(3939, () => console.log('Server running on port 3939'));
+app.listen(3000, () => console.log('Server running on port 3000'));
